096296a2c7747fabc378630f946cc38d4f96175a
Indumathi2468 <indumathin339@gmail.com>
2025-10-28 20:27:11 +0530

    Add files via upload

---
+++ main.2.py
@@ -0,0 +1,10 @@
+# main.py
+import medical_data_visualizer
+from unittest import main
+
+# Test your function by calling them here
+medical_data_visualizer.draw_cat_plot()
+medical_data_visualizer.draw_heat_map()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
